55fd42074968af936450ca3496803fe37300ef01
Matt Huntington <matt.huntington@gmail.com>
2016-08-22 14:48:21 -0400

    started server listening

---
+++ server.js
@@ -0,0 +1,8 @@
+var express = require('express'); //require express package
+var app = express(); //create application variable
+
+//params: port, callback to execute once listening has begun
+app.listen(3000, function () { //start app listening on port 3000
+	//once listening, execute this callback
+	console.log('Example app listening on port 3000!');
+});
