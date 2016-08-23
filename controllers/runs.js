6b2fc89a5ac656a9a8abfb09986282214e108fa6
Matt Huntington <matt.huntington@gmail.com>
2016-08-23 09:59:59 -0400

    move new route up

--- controllers/runs.js
+++ controllers/runs.js
@@ -6,12 +6,12 @@ controller.get('/', function(req, res){
 	res.send('run index page');
 });
 
-controller.get('/:id', function(req, res){
-	res.send('run show page for id: ' +  req.params.id);
-});
-
 controller.get('/new', function(req, res){
 	res.send('run new page');
+});
+
+controller.get('/:id', function(req, res){
+	res.send('run show page for id: ' +  req.params.id);
 });
 
 module.exports = controller;
