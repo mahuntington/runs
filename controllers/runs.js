b352703da24d703b1c189d394e1f5399409807a1
Matt Huntington <matt.huntington@gmail.com>
2016-08-23 09:46:37 -0400

    run show page

--- controllers/runs.js
+++ controllers/runs.js
@@ -2,11 +2,15 @@ var express = require('express');
 var controller = express.Router();
 
 controller.get('/', function(req, res){
-	res.send('inside runs controller: index route');
+	res.send('run index page');
+});
+
+controller.get('/:id', function(req, res){
+	res.send('run show page for id: ' +  req.params.id);
 });
 
 controller.get('/new', function(req, res){
-	res.send('inside runs controller: new route');
+	res.send('run new page');
 });
 
 module.exports = controller;
