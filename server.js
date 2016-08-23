1ff2b714dfa8aca250ea390042f71c9a28cb2775
Matt Huntington <matt.huntington@gmail.com>
2016-08-23 09:02:56 -0400

    middleware and extra route

--- server.js
+++ server.js
@@ -3,13 +3,30 @@ var app = express(); //create application variable
 var fakeArray = require('./models/fakearray.js');
 var PORT = process.env.PORT || 3000;
 
-console.log(fakeArray);
+app.use(function(req, res, next){
+	console.log('middleware doin. stuff');
+	next();
+});
+
+app.use(function(req, res, next){
+	console.log('2nd middleware doin. stuff');
+	next();
+});
+
+app.use(function(req, res, next){
+	console.log('3rd middleware doin. stuff');
+	next();
+});
 
 app.get('/', function(request, response){
 	response.render('index.ejs', {
 		dataArray: fakeArray
 	});
 });
+
+app.get('/foo', function(req, res){
+	res.send('works');
+})
 
 //params: port, callback to execute once listening has begun
 app.listen(PORT, function () { //start app listening on port 3000
