13b03e3f6c5f72d82634383dab58e291fce66900
Matt Huntington <matt.huntington@gmail.com>
2016-08-25 10:57:52 -0400

    check username/password of req.body with users in db

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var controller = express.Router();
+var Users = require('../models/users.js');
 
 controller.use(bodyParser.urlencoded({extended:true}));
 
@@ -9,7 +10,17 @@ controller.get('/new', function(req, res){
 });
 
 controller.post('/', function(req, res){
-	res.send(req.body);
+	Users.find({
+		where: {
+			name: req.body.name
+		}
+	}).then(function(foundUser){
+		if(foundUser.password == req.body.password){
+			res.redirect('/');
+		} else {
+			res.redirect('/sessions/new');
+		}
+	});
 });
 
 module.exports = controller;
