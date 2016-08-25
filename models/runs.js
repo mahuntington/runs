aea97f01fc3484d6cd711952fb6614bc00b8661f
Matt Huntington <matt.huntington@gmail.com>
2016-08-25 09:40:48 -0400

    oops

--- models/runs.js
+++ models/runs.js
@@ -3,7 +3,7 @@ var db = require('../models/db_connection.js'); //require connection to the db
 
 var Run = db.define('run', { //set up model variables
     date: Sequelize.DATE, //use date data type
-    distance: Sequelize.FLOAT, //float for distance
+    distance: Sequelize.FLOAT //float for distance
 });
 
 db.sync(); //if table does not exist, create it
